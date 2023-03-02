60eaa1cbb2d79e44c3d92f05cc95832f53c8051b
issywallwork <issy.wallwork@matillion.com>
2023-03-02 18:01:52 +0000

    Initial commit.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,8 +4,5 @@ public class main {
     public static void main(String[]args){
         System.out.println(new One(3, 5).result());
         System.out.println(new Two().evens());
-//        Three three = new Three();
-//        Four four = new Four();
-//        Five five = new Five();
     }
 }
